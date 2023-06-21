ea31f95fde9cbdd3fbd1c73abe6a769b16646711
Chris <17653365+odcambc@users.noreply.github.com>
2023-06-21 08:33:25 -0700

    Initial commit

---
+++ od_standards_calibration_plugin/__init__.py
@@ -0,0 +1,3 @@
+from od_standards_calibration_plugin.od_calibration_from_standards import (
+    click_od_calibration_from_standards,
+)
---
+++ setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name="od_standards_calibration_plugin",
+    version="1.0.0",
+    license="MIT",
+    description="Calibrate OD using a set of standards.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author_email="christian.macdonald@ucsf.edu",
+    author="Chris Macdonald",
+    url="https://github.com/odcambc/od_standards_calibration_plugin",
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        "pioreactor.plugins": "od_standards_calibration_plugin = od_standards_calibration_plugin"
+    },
+)
